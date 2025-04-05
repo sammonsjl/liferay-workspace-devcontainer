4fb948114b2512fa72cbbe7df7e88f3c0985b104
Jamie Sammons <jamie.sammons@liferay.com>
2025-04-04 22:25:30 -0500

    Liferay Workspace devcontainer templete

--- .devcontainer/generator-liferay-theme/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Installing Liferay Theme Generator"
-
-npm install -g yo@4.x.x gulp generator-liferay-theme@10
--- .devcontainer/liferay-blade-cli/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Installing Liferay Blade CLI"
-
-curl -L https://raw.githubusercontent.com/liferay/liferay-blade-cli/master/cli/installers/local | sh
