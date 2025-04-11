fb32986d0c4ac693ff16245751c00ff22b31bb85
Jamie Sammons <jamie.sammons@liferay.com>
2025-04-11 11:32:25 -0500

    Fixes

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,8 +1,8 @@
+PUBLIC_KEY=$(ssh-add -L | grep "Github")
+
 git config --global gpg.format ssh
 git config --global commit.gpgsign true
+git config --global user.signingkey "$PUBLIC_KEY"
 git config --global --unset gpg.ssh.program
 
-PUBLIC_KEY=$(ssh-add -L | grep "Github")
-git config --global user.signingkey "$PUBLIC_KEY"
-
-git config --global user.signingkey "$(ssh-add -L)"
+blade gw clean deploy
--- .devcontainer/liferay/override/scripts/restore.sh
+++ .devcontainer/liferay/override/scripts/restore.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
 restore() {
-for f in /docker-entrypoint-initlr.d/*; do
-	echo "$f"
-	case "$f" in
-	  *.gz) echo "$0: running $f"; gunzip -c "$f" | mysql -u "$LIFERAY_DATABASE_USER" -h database; echo ;;
-	  *.tgz) echo "$0: running $f"; tar -xzf "$f" -C /opt/liferay/data; echo ;;
-	esac
-	echo
-done
+	for f in /docker-entrypoint-initlr.d/*; do
+		echo "$f"
+		case "$f" in
+		*.gz)
+			echo "$0: running $f"
+			gunzip -c "$f" | mysql -u "$LIFERAY_DATABASE_USER" -h database
+			echo
+			;;
+		*.tgz)
+			echo "$0: running $f"
+			tar -xzf "$f" -C /opt/liferay/data
+			echo
+			;;
+		esac
+		echo
+	done
 
-if [[ -f /docker-entrypoint-initlr.d/z-cleanup-data.sql ]]
-then
-	mysql -u "$LIFERAY_DATABASE_USER" -h database < /docker-entrypoint-initlr.d/z-cleanup-data.sql
-fi
+	if [[ -f /docker-entrypoint-initlr.d/z-cleanup-data.sql ]]; then
+		mysql -u "$LIFERAY_DATABASE_USER" -h database </docker-entrypoint-initlr.d/z-cleanup-data.sql
+	fi
 
-touch $LIFERAY_HOME/data/.backup-restored
-
-cp /docker-entrypoint-initlr.d/license.xml /opt/liferay/deploy/
+	touch $LIFERAY_HOME/data/.backup-restored
 }
 
 if [[ ! -f "$LIFERAY_HOME/data/.backup-restored" ]]; then
-restore
+	restore
 fi
+
+cp /docker-entrypoint-initlr.d/deploy/* /opt/liferay/deploy/
