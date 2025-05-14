ff5d66f55f28b92d490bd0c2448cbaec92c81bd6
Jamie Sammons <jamie.sammons@liferay.com>
2025-05-14 22:33:39 +0000

    cleanup

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,7 +1,3 @@
-#pushd .. >/dev/null
-
 docker run --rm -v ./:/workspace -w /workspace \
   --user root liferay/jdk21:latest \
   ./gradlew --gradle-user-home=/home/liferay/.gradle clean createDockerfile
-
-#popd >/dev/null
